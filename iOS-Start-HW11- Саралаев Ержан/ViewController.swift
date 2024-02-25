f5c222f5dd44fba9b51a1f4ffcbddbb0766a6508
Erzhan <saralaev03@mail.ru>
2024-02-25 20:00:51 +0600

    Add loginTextField

--- iOS-Start-HW11- Саралаев Ержан/ViewController.swift
+++ iOS-Start-HW11- Саралаев Ержан/ViewController.swift
@@ -8,10 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passworldTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // MARK: - loginField
+        loginTextField.layer.shadowColor = UIColor.black.cgColor
+        loginTextField.layer.shadowOpacity = 0.3
+        loginTextField.layer.shadowOffset = .zero
+        loginTextField.layer.shadowRadius = 10
+        loginTextField.layer.shouldRasterize = true
+        loginTextField.layer.rasterizationScale = UIScreen.main.scale
+        loginTextField.textColor = .gray
+        loginTextField.textAlignment = .center
+        loginTextField.placeholder = "Email"
+        loginTextField.layer.borderColor = UIColor.black.cgColor
+        loginTextField.clipsToBounds = true
+        loginTextField.layer.cornerRadius = 20
+        loginTextField.layer.borderWidth = 0.2
+        loginTextField.layer.masksToBounds = true
+        
     }
 
 
